0ac30f210dac8c1c69bec06a9989248461713978
thebuilderekes <ekeopreberedugo1@gmail.com>
2024-07-03 19:16:34 +0100

    fix: changed technology section bg color

--- js/animation.js
+++
@@ -1,5 +0,0 @@
-ScrollReveal({ reset: true });
-ScrollReveal().reveal(".headline", { duration: 500 });
-ScrollReveal().reveal(".punchline", { delay: 700 });
-ScrollReveal().reveal(".tagline", { delay: 700 });
-ScrollReveal().reveal(".widget", { interval: 700 });
